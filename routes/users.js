ecace3b54fadc4622d3fb15d8e3249df32c100a8
BrenStro <Brendon.Strowe@iCloud.com>
2017-04-16 16:21:26 -0400

    Added User script which reflects the User table in the database. Updated users.js route scipt to allow for requesting a User entry from the database by its 'id' property.

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 var MySQLDatabase = require('../db/MySQLDatabase.js');
+var User = require('../db/User.js');
 
 /* GET users listing. */
 router.get('/', function (request, response, next) {
 
 	MySQLDatabase.getData("SELECT id, firstName, lastName, email FROM User").then(function (resultSet) {
 		response.send(resultSet.rows);
+	}).catch(function(error) {
+		response.send(error);
+	});
+});
+
+/* GET a User by its id */
+router.get('/:id', function (request, response) {
+	let requestedUser = new User(request.params.id);
+
+	// Fetch the requested user. Respond with data about the user.
+	//  If there is an error, catch it and report it.
+	requestedUser.fetch().then(function() {
+		response.send(requestedUser.id + " " + requestedUser.firstName + " " + requestedUser.lastName);
 	}).catch(function(error) {
 		response.send(error);
 	});
