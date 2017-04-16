960a45851a2aad1fb6fb01623eba218e3b98454b
BrenStro <Brendon.Strowe@iCloud.com>
2017-04-16 13:44:12 -0400

    Updated package.json to include the dependency on the 'mysql' package. Created MySQLDatabase script for connecting to a MySQL database. Updated routes/users.js to interact with a MySQL Database using the MySQLDatabase script.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var MySQLDatabase = require('../db/MySQLDatabase.js');
+
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+router.get('/', function (request, response, next) {
+
+	MySQLDatabase.getData("SELECT id, firstName, lastName, email FROM User").then(function (resultSet) {
+		response.send(resultSet.rows);
+	}).catch(function(error) {
+		response.send(error);
+	});
 });
 
 module.exports = router;
