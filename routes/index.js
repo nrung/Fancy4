4579e6b0d2cc2321ba436c3f81dc104a971a4869
BrenStro <Brendon.Strowe@iCloud.com>
2017-04-16 16:03:14 -0400

    Added Paper script which reflects the Papers table in the database. Updated the index.js script to allow for requesting a Paper entry from the database by its 'id' property.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var Paper = require('../db/Paper');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
+});
+
+/* GET a Paper by its id */
+router.get('/Papers/:id', function (request, response) {
+	let requestedPaper = new Paper(request.params.id);
+
+	// Fetch the requested paper. Respond with data about the paper.
+	//  If there is an error, catch it and report it.
+	requestedPaper.fetch().then(function() {
+		response.send(requestedPaper.id + " " + requestedPaper.title);
+	}).catch(function(error) {
+		response.send(error);
+	});
 });
 
 module.exports = router;
