534faccd183c7cd623c7db3c0a1457026b0c38cf
BrenStro <Brendon.Strowe@iCloud.com>
2017-04-17 23:35:32 -0400

    Added PaperKeywords script which reflects the PaperKeywords table in the database. Updated index.js route scipt to allow for requesting the Keywords of a Paper entry from the database by its 'id' property.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const Paper = require('../db/Paper.js');
+const PaperKeywords = require('../db/PaperKeywords.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,13 +10,26 @@ router.get('/', function (req, res, next) {
 });
 
 /* GET a Paper by its id */
-router.get('/Papers/:id', function (request, response) {
+router.get('/papers/:id', function (request, response) {
 	let requestedPaper = new Paper(request.params.id);
 
 	// Fetch the requested paper. Respond with data about the paper.
 	//  If there is an error, catch it and report it.
 	requestedPaper.fetch().then(function() {
 		response.send(requestedPaper.id + " " + requestedPaper.title);
+	}).catch(function(error) {
+		response.send(error);
+	});
+});
+
+/* GET a Paper's keywords by its id */
+router.get('/papers/:id/keywords', function (request, response) {
+	let requestedPaperKeywords = new PaperKeywords(request.params.id);
+
+	// Fetch the requested paper. Respond with data about the paper.
+	//  If there is an error, catch it and report it.
+	requestedPaperKeywords.fetch().then(function() {
+		response.send(requestedPaperKeywords.paperId + " " + requestedPaperKeywords.keywords);
 	}).catch(function(error) {
 		response.send(error);
 	});
